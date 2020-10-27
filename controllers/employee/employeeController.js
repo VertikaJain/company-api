1ab1f8d1cb2665030a54a7288d395e88030e38d0
Vertika Jain <vertikajain96@gmail.com>
2020-10-27 20:03:00 +0530

    Added Employee Validation with Status Codes

--- controllers/employee/employeeController.js
+++ controllers/employee/employeeController.js
@@ -5,19 +5,34 @@ employeeController = () => {
         async index(req, res) {
             // return list of employees
             const employees = await Employees.find();
-            res.json(employees)
+            res.status(200).json(employees)
         },
         addEmployee(req, res) {
             // add employee to database
             const { name, email, phone, project } = req.body
             const employee = new Employees({ name, email, phone, project });
             employee.employeeId = employee._id
+
+            // Validation
+            if (!name || !email || !phone || !project) {
+                return res.status(400).json({ err: "All fields are required." })
+            }
+            if (phone.length != 10) {
+                return res.status(400).send({ error: "Invalid phone number." })
+            }
+            // Check if email id exists
+            Employees.exists({ email: email }, (err, result) => {
+                if (result) {
+                    return res.status(400).send({ error: "Email ID already exists." })
+                }
+            })
+
             // Saving employee data to MongoDB
             employee.save().then(() => {
-                res.json({ employee })
+                res.status(201).json({ employee })
 
             }).catch(err => {
-                res.json({ err })
+                res.status(400).json({ err })
             })
         }
     }
